b8a9738d7e39f87469e033e76dce67037d67652a
zac1st1k <zacfirst@gmail.com>
2015-02-26 23:49:46 +1100

    Add dynamic menu

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,4 +2,14 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # return current user if existing
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+	# if logged in return ture else return false 
+  def logged_in?
+    !current_user.nil?
+  end
 end
