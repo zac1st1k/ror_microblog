fdfefb23b50d1ef8fd4fa897fe3936455213a9f8
zac1st1k <zacfirst@gmail.com>
2015-02-26 23:02:07 +1100

    Add log-in flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+		# login, redirect user profile page
+	  else
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new'
  	  end
   end
 
