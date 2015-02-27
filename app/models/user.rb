27bd5f63bc72dc2a5818926be9b8cf3d8cec00c0
zac1st1k <zacfirst@gmail.com>
2015-02-27 20:13:26 +1100

    Add log-in integration test

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,11 @@ class User < ActiveRecord::Base
   									uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  # return string hash digest
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
