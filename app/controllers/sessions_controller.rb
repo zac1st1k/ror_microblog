d450d475568bf56714e15d0d83a827e7c3aaa525
zac1st1k <zacfirst@gmail.com>
2015-02-27 21:17:13 +1100

    Add cookie token for persistent session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
 		# login, redirect user profile page
       log_in user
+      remember user
       redirect_to user
 	  else
       flash.now[:danger] = 'Invalid email/password combination'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,9 +3,23 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  # return current user if existing
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # return matched user by token in cookie
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
 	# if logged in return ture else return false 
