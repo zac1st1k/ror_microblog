0ecc1957875e21d0831f05867e8c05b20c3abfa9
zac1st1k <zacfirst@gmail.com>
2015-02-28 23:49:38 +1100

    Add edit page sucessful test

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   									format: { with: VALID_EMAIL_REGEX }, 
   									uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, allow_blank: true
 
   # return string hash digest
   def User.digest(string)
