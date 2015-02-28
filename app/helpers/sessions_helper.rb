36173512564684354a5a034a8e5339c9e085d96f
zac1st1k <zacfirst@gmail.com>
2015-02-28 16:31:44 +1100

    Add remember me test

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,11 +9,12 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-  # return matched user by token in cookie
+  # return matched user by token from cookie
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
+
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,7 +10,29 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  # return true if logged in
   def is_logged_in?
     !session[:user_id].nil?
   end
+  
+  # logged in user test
+  def log_in_as(user, options = {})
+    password    = options[:password]    || 'password'
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      post login_path, session: { email:       user.email,
+                                  password:    password,
+                                  remember_me: remember_me }
+    else
+      session[:user_id] = user.id
+    end
+  end
+
+  private
+
+    # return true in integration test
+    def integration_test?
+      defined?(post_via_redirect)
+    end
 end
