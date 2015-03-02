5ae0b559a25c1ebd37a44399909d7c65412a6f55
zac1st1k <zacfirst@gmail.com>
2015-03-02 15:29:25 +1100

    Add logged in users layout test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,10 +1,40 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
 
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+  end
+
+  test "layout links for logged-in users" do
+    get login_path
+    post login_path, session: { email: @user.email, password: 'password' }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+  end
+
+  test "layout links for non-logged-in users" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", login_path, count: 1
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
