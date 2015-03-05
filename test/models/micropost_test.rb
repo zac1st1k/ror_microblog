34d1ddb144b3bb4bf88f45782276cc5ca0f1421b
zac1st1k <zacfirst@gmail.com>
2015-03-05 23:50:05 +1100

    Add micropost refinement and test

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,8 +4,7 @@ class MicropostTest < ActiveSupport::TestCase
 
   def setup
     @user = users(:michael)
-    # This code is not idiomatically correct.
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
@@ -16,7 +15,7 @@ class MicropostTest < ActiveSupport::TestCase
     @micropost.user_id = nil
     assert_not @micropost.valid?
   end
-  
+
   test "content should be present " do
     @micropost.content = "   "
     assert_not @micropost.valid?
@@ -25,5 +24,9 @@ class MicropostTest < ActiveSupport::TestCase
   test "content should be at most 140 characters" do
     @micropost.content = "a" * 141
     assert_not @micropost.valid?
+  end
+  
+  test "order should be most recent first" do
+    assert_equal Micropost.first, microposts(:most_recent)
   end
 end
